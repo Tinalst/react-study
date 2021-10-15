de96d3072380b0c00ecca699b904c93c8e174798
tinalst <lst_tina@163.com>
2021-10-15 19:07:39 +0800

    feat: nesting-routing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
-import {BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom'
+import {Link, Switch, Route, useRouteMatch, BrowserRouter as Router} from "react-router-dom";
 import * as ReactDOM from "react-dom";
+
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to={"/"}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to={"/about"}>About</Link>
+                    </li>
+                    <li>
+                        <Link to={"/topics"}>Topics</Link>
+                    </li>
+                </ul>
+
+                {/*此处是路由的视图窗口，要写到div里*/}
+                <Switch>
+                    <Route path={"/about"}>
+                        <About/>
+                    </Route>
+                    <Route path={"/topics"}>
+                        <Topics />
+                    </Route>
+                    {/* 这个要放到最后，不然视图切换不起来！！！！！！*/}
+                    <Route path={"/"}>
+                        <Home/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    )
+}
+
 
 function Home() {
     return <h2>Home</h2>
@@ -9,42 +45,26 @@ function About() {
     return <h2>About</h2>
 }
 
-function Users() {
-    return <h2>Users</h2>
-}
+function Topics() {
+    // let match = useRouteMatch()
+    // console.log(match);
 
-function App() {
     return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to={"/"}>Home</Link>
-                        </li>
-                        <li>
-                            <Link to={"about"}>Users</Link>
-                        </li>
-                        <li>
-                            <Link to={"users"}>Users</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+        <div>
+            <h2>Topics</h2>
+            <ul>
+                <li>
+                    {/*<Link>Components</Link>*/}
+                </li>
+                <li>
 
-            {/*下面这个是展示*/}
-            <Switch>
-                <Route path={"/about"}>
-                    <About/>
-                </Route>
-                <Route path={"/users"}>
-                    <Users/>
-                </Route>
-                <Route path={"/"}>
-                    <Home/>
-                </Route>
-            </Switch>
-        </Router>
+                </li>
+            </ul>
+        </div>
+
+        // <Switch>
+        //     <Route path={}></Route>
+        // </Switch>
     )
 }
 
