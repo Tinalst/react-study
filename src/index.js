7edbfdf0783b1769d5a8b3509c41886cbf41cafb
tinalst <lst_tina@163.com>
2021-10-16 09:47:03 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,41 +1,9 @@
-import ThemeTogglerButton from "./theme-toggler-button";
 import React from "react";
-import {ThemeContext, themes} from "./theme-context";
 
-function Content() {
-    return (
-        <div>
-            <ThemeTogglerButton/>
-        </div>
-    )
-}
+// 创建一个context
+const ThemeContext = React.createContext('light')
+// 创建一个context
+const UserContext = React.createContext({
+    name: 'Guest'
+})
 
-
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.toggleTheme = () => {
-            this.setState((state) => ({
-                theme: state.theme === themes.dark
-                    ? themes.light
-                    : themes.dark
-            }))
-        }
-
-        this.state = {
-            theme: themes.light,
-            toggleTheme: this.toggleTheme
-        }
-    }
-
-
-    render() {
-        return (
-            // 这样子 就能将this.state 传递给 Content下的 ThemeTogglerButton组件了
-            <ThemeContext.Provider value={this.state}>
-                <Content/>
-            </ThemeContext.Provider>
-        )
-    }
-}
