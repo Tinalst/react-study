430853b36f2c302f6fa4527350de2042e55ebc2c
tinalst <lst_tina@163.com>
2021-10-16 10:06:17 +0800

    feat: ref-basic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from "react";
+import * as ReactDOM from "react-dom";
 
-// 创建一个context
-const ThemeContext = React.createContext('light')
-// 创建一个context
-const UserContext = React.createContext({
-    name: 'Guest'
-})
+// 定义组件
+const FancyButton = React.forwardRef(((props, ref1) => {
+    return (
+        <button ref={ref1}>
+            {props.children}
+        </button>
+    )
+}))
 
+// 1. 创建react.ref
+const ref = React.createRef();
+
+//2. ref变量传递给组件
+ReactDOM.render(
+    // 传递ref变量值给到组件的forwardRef参数
+    <FancyButton ref={ref}>click me</FancyButton>,
+    document.getElementById('root')
+)
