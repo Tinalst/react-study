a9f42014efabd8008e850164ba5fc86c84ac1b8c
tinalst <lst_tina@163.com>
2021-10-19 16:16:07 +0800

    feat: props render

--- src/index.js
+++ src/index.js
@@ -1,37 +1,51 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
 
-class CustomTextInput extends React.Component {
+
+class Cat extends React.Component {
+    render () {
+        const mouse = this.props.mouse;
+        return (
+            <img src="/cat.jpg" style={{ position: 'absolute', left: mouse.x, top: mouse.y }} />
+        )
+    }
+}
+
+class Mouse extends React.Component {
     constructor(props) {
         super(props);
-
-        // ref 回调写法
-        this.setTextInputRef = el => {
-            this.textInput  = el
-        }
-
-        this.focusTextInput = () => {
-            if(this.textInput) this.textInput.focus()
+        this.state = {
+            x: 0,
+            y: 0
         }
     }
 
-
-    componentDidMount() {
-        this.focusTextInput();
+    handleMouseMove(e) {
+        this.setState({
+            x: e.clientX,
+            y: e.clientY
+        })
     }
-
 
     render() {
         return (
+            <div style={{height: '100vh'}} onMouseMove={e => this.handleMouseMove(e)} >
+                {/*this.props.render(this.state) 返回了CAT组件*/}
+                {this.props.render(this.state)}
+            </div>
+        )
+    }
+}
+
+class MouseTracker extends React.Component {
+    render() {
+        return (
             <div>
-                <input
-                    type={"text"}
-                    ref={this.setTextInputRef}
-                />
-                <input
-                    type={"button"}
-                    value={"click"}
-                    onClick={this.focusTextInput}
+                鼠标移动
+                <Mouse
+                    render={mouse => (
+                        <Cat mouse={mouse}/>
+                    )}
                 />
             </div>
         )
@@ -39,6 +53,6 @@ class CustomTextInput extends React.Component {
 }
 
 ReactDOM.render(
-    <CustomTextInput/>,
+    <MouseTracker/>,
     document.getElementById('root')
 )
