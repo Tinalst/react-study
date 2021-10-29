449ef6f0141bbbd93ad4ca11f5d0c47c42a648b8
tinalst <lst_tina@163.com>
2021-10-29 10:07:00 +0800

    observable-state

--- src/index.js
+++ src/index.js
@@ -1,105 +1,39 @@
+import {makeAutoObservable} from "mobx";
+import {observer} from "mobx-react-lite";
 import * as ReactDOM from "react-dom";
 
-import {action, computed, makeObservable, observable} from "mobx";
-import {observer} from "mobx-react-lite"
 
-
-class ObservableTodoStore {
-    todos = [];
-    pendingRequests = 0;
+class Timer {
+    secondsPassed = 0
 
     constructor() {
-        // 创建可跟踪的对象
-        makeObservable(this, {
-            todos: observable,
-            pendingRequests: observable,
-            addTodo: action,
-            completedTodosCount: computed,
-            report: computed // 创建可从状态自动获得值并缓存
-        })
+        makeAutoObservable(this)
     }
 
-    // define action
-    addTodo(task) {
-        this.todos.push({
-            task: task,
-            completed: false,
-            assignee: null
-        })
-    }
-
-    // define computed
-
-    get completedTodosCount() {
-        return this.todos.filter(
-            todo => todo.completed === true
-        ).length;
-    }
-
-    get report() {
-        if (this.todos.length === 0)
-            return "<none>";
-        const nextTodo = this.todos.find(todo => todo.completed === false);
-        return `Next todo: "${nextTodo ? nextTodo.task : "<none>"}". ` +
-            `Progress: ${this.completedTodosCount}/${this.todos.length}`;
+    increaseTimer() {
+        this.secondsPassed += 1
     }
 }
 
-const observableTodoStore = new ObservableTodoStore();
+const timer = new Timer();
 
-
-// observer使组件具有reaction特性
-const TodoList = observer(({store}) => {
-    const onNewTodo = () => {
-        store.addTodo(prompt('Enter a new todo:','coffee plz'));
+const TimerView = observer(
+    ({timer}) => {
+        return (
+            <span>
+                seconds passed: {timer.secondsPassed}
+            </span>
+        )
     }
-
-    return (
-        <div>
-            { store.report }
-            <ul>
-                { store.todos.map(
-                    (todo, idx) => <TodoView todo={ todo } key={ idx } />
-                ) }
-            </ul>
-            { store.pendingRequests > 0 ? <marquee>Loading...</marquee> : null }
-            <button onClick={ onNewTodo }>New Todo</button>
-            <small> (double-click a todo to edit)</small>
-            {/*<RenderCounter />*/}
-        </div>
-    );
-})
-
-
-const TodoView = observer(({todo}) => {
-    const onToggleCompleted = () => {
-        todo.completed = !todo.completed;
-    }
-
-    const onRename = () => {
-        todo.task = prompt('Task name', todo.task) || todo.task;
-    }
-
-    return (
-        <li onDoubleClick={ onRename }>
-            <input
-                type='checkbox'
-                checked={ todo.completed }
-                onChange={ onToggleCompleted }
-            />
-            { todo.task }
-            { todo.assignee
-                ? <small>{ todo.assignee.name }</small>
-                : null
-            }
-            {/*<RenderCounter />*/}
-        </li>
-    );
-})
-
-
+)
 
 ReactDOM.render(
-    <TodoList store={observableTodoStore}/>,
+    <TimerView timer={timer}/>,
     document.getElementById('root')
 )
+
+
+// test observable state
+setInterval(() => {
+    timer.increaseTimer()
+}, 1000)
